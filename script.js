8f2ec590c99d59f036e8df237337b26c028ba4c9
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-24 23:34:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let randomString="";
+	for(let i=0; i<l; i++){
+		let randomIndex = Math.floor(Math.random() * characters.length);
+		randomString += characters.charAt(randomIndex);
+	}
+	return randomString;
+	
 }
 
 // Do not change the code below.
